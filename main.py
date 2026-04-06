f4ea732933ea04bb8c9d59592fafafe09775ef15
Diego Lozoya Morales <diego.lozoya@gmail.com>
2026-04-06 01:19:19 -0600

    Iimplemented plt.rcParams rules, colors to use and a color scale for consistency in all graphs

--- main.py
+++ main.py
@@ -1,14 +1,76 @@
 import pandas as pd
 
+from fitter import model_fitter
+from utils import split_scale_pca
+from CreditModel import get_predictions, compute_expected_pnl, realized_pnl
 from visualization import plot_interest_rate_distribution
 
 
 def main():
-    # Load datasets
+    # === Load datasets ===
     data = pd.read_csv('Data/UCI_Credit_Card.csv')
     interest_rate_df = pd.read_csv('Data/InterestRate.csv')
     
-    # Plots
+    # === Define target and features ===
+    target = 'default.payment.next.month'
+    y = data[target]
+    X = data.copy().drop(columns=['ID', target])
+    
+    # === Data preprocessing ===
+    results = split_scale_pca(X, y, interest_rate_df['TotalInterestRate'])
+    
+    X_train, X_val, X_test = results["X_train"], results["X_val"], results["X_test"]
+    y_train, y_val, y_test = results["y_train"], results["y_val"], results["y_test"]
+    r_train, r_val, r_test = results["r_train"], results["r_val"], results["r_test"]
+    EAD_train, EAD_val, EAD_test = results["EAD_train"], results["EAD_val"], results["EAD_test"]
+    
+    # === Model fitting ===
+    # De-comment the line below to fit the model and save it to disk.
+    # model_fitter(X_train, y_train)
+    
+    # === Credit Model ===
+    # Load fitted model
+    model = pd.read_pickle('Model/xgb_model.pkl')
+    
+    # --- Predictions ---
+    train_probabilities, train_predictions, train_thresholds = get_predictions(model, X_train, r_train)
+    val_probabilities, val_predictions, val_thresholds = get_predictions(model, X_val, r_val)
+    #test_probabilities, test_predictions, test_thresholds = get_predictions(model, X_test, r_test)
+    
+    # --- Expected PnL Calculation ---
+    train_expected_pnl = compute_expected_pnl(train_probabilities, r_train, EAD_train)
+    val_expected_pnl = compute_expected_pnl(val_probabilities, r_val, EAD_val)
+    #test_expected_pnl = compute_expected_pnl(test_probabilities, r_test, EAD_test)
+    
+    # --- Realized PnL Calculation ---
+    train_realized_pnl = realized_pnl(r_train, EAD_train, train_predictions, y_train)
+    val_realized_pnl = realized_pnl(r_val, EAD_val, val_predictions, y_val)
+    #test_realized_pnl = realized_pnl(r_test, EAD_test, test_predictions, y_test)
+    
+    # NOT FINAL RESULTS - JUST QUICK CHECKS
+    print(f'Total Expected PnL (Train): {train_expected_pnl.sum():,.2f}')
+    print(f'Total Realized PnL (Train): {train_realized_pnl.sum():,.2f}')
+    
+    print(f'Total Expected PnL (Validation): {val_expected_pnl.sum():,.2f}')
+    print(f'Total Realized PnL (Validation): {val_realized_pnl.sum():,.2f}')
+    
+    print('train:')
+    approved = (train_predictions == 1)
+    print(f"Default rate among approved: {y_train[approved].mean():.1%}")
+    print(f"Overall default rate: {y_train.mean():.1%}")
+    print(f"Approval rate: {train_predictions.mean():.1%}")
+    print(f"Mean threshold: {train_thresholds.mean():.3f}")
+    print(f"Clients above threshold: {(y_train > train_thresholds).mean():.1%}")
+
+    print('validation:')
+    approved = (val_predictions == 1)
+    print(f"Default rate among approved: {y_val[approved].mean():.1%}")
+    print(f"Overall default rate: {y_val.mean():.1%}")
+    print(f"Approval rate: {val_predictions.mean():.1%})")
+    print(f"Mean threshold: {val_thresholds.mean():.3f}")
+    print(f"Clients above threshold: {(y_val > val_thresholds).mean():.1%}")
+    
+    # === Plots ===
     plot_interest_rate_distribution(interest_rate_df)
     
 
--- visualization.py
+++ visualization.py
@@ -1,15 +1,48 @@
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import ConfusionMatrixDisplay
+
+plt.rcParams['figure.figsize'] = (12, 5)
+plt.rcParams['axes.grid'] = True
+plt.rcParams['grid.alpha'] = 0.5
+plt.rcParams['grid.linestyle'] = '--'
+plt.rcParams['axes.titlesize'] = 14
+plt.rcParams['axes.labelsize'] = 12
+plt.rcParams['legend.frameon'] = True
+plt.rcParams['legend.facecolor'] = 'white'
+plt.rcParams['legend.edgecolor'] = 'black'
+plt.rcParams['legend.loc'] = 'best'
+
+blue_colors = [
+    "#D9EDFF",
+    "#C1E2FF",
+    "#A8D5FF",
+    "#8AC6FF",
+    "#6BB6FF",
+    "#4A9EE5",
+    "#2E86DE",
+    "#0066C0",
+    "#0055A5",
+    "#003D82"
+]
+
+blue_scale = mcolors.LinearSegmentedColormap.from_list(
+    "blue_scale",
+    [blue_colors[0], blue_colors[-1]]
+)
 
 
 def plot_interest_rate_distribution(interest_rate_df):
-    plt.figure(figsize=(12, 6))
-    sns.kdeplot(interest_rate_df['TotalInterestRate'], fill=True, color='navy')
-    plt.axvline(interest_rate_df['TotalInterestRate'].mean(), color='indianred', 
+    plt.figure()
+    sns.kdeplot(interest_rate_df['TotalInterestRate'], fill=True, color=blue_colors[-1])
+    plt.axvline(interest_rate_df['TotalInterestRate'].mean(), color='dimgray', 
                 linestyle='--', label=f'Mean Total Interest Rate: {interest_rate_df['TotalInterestRate'].mean():.2%}')
     plt.title('Distribution of Total Interest Rates')
     plt.xlabel('Total Interest Rate')
     plt.ylabel('Density')
-    plt.grid(linestyle='--', alpha=0.5)
+    plt.grid()
     plt.legend()
     plt.show()
